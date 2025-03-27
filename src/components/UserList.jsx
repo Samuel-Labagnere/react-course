a03319f98a6271db48a605af32c3c4c337333932
Samuel-Labagnere <samuel.labagnere@gmail.com>
2025-03-27 17:14:43 +0100

    Fixed avatars with lower resolutions

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,7 +3,7 @@ const UserList = ({ users = [] }) => {
     <ul className="grid grid-cols-4 gap-3">
       {users.map((user) => (
         <li key={user.id} className="flex flex-col items-center">
-          <img src={user.avatar_url} />
+          <img src={user.avatar_url} className="h-full w-full object-cover" />
           <p>&lt;{user.login}&gt;</p>
         </li>
       ))}
